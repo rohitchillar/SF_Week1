bf2ec0f673326ed94bcf8c1427faf86a671e0a89
Rohit <rohit.chillar31@gmail.com>
2022-02-13 17:51:01 +0530

    Crowd source platform addition and first deploy

---
+++ Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-1f9d6eafb693d297.d
@@ -0,0 +1,5 @@
+/home/loopmonkey/Desktop/SF_Week1/Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-1f9d6eafb693d297.rmeta: src/lib.rs
+
+/home/loopmonkey/Desktop/SF_Week1/Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-1f9d6eafb693d297.d: src/lib.rs
+
+src/lib.rs:
---
+++ Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-b876a97a4dd3098d.d
@@ -0,0 +1,5 @@
+/home/loopmonkey/Desktop/SF_Week1/Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-b876a97a4dd3098d.rmeta: src/lib.rs
+
+/home/loopmonkey/Desktop/SF_Week1/Deploy a program/crowdfunding_platform/target/rls/debug/deps/crowdsource-b876a97a4dd3098d.d: src/lib.rs
+
+src/lib.rs:
